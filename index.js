49830eeabeed181a984ec6694710493153d8f40e
Kevin Siqueira <90641543+Kzer05@users.noreply.github.com>
2025-11-03 21:12:23 -0300

    Porta utilizada pelo render

--- index.js
+++ index.js
@@ -4,25 +4,23 @@ const cors = require('cors');
 const habitRoutes = require('./habitController');
 
 const app = express();
-const PORT = 3000;
 
 app.use(cors());
-
 app.use(express.json({
     verify: (req, _, buf) => {
         req.rawBody = buf && buf.toString ? buf.toString() : '';
     }
 }));
 
-// Log global para ver requisições
 app.use((req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
 });
 
-// Rotas
 app.use('/', habitRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
