6e3b10a74165bdd8ed96fe7c14b0150c46d5ec7f
Kevin Siqueira <90641543+Kzer05@users.noreply.github.com>
2025-11-03 21:35:13 -0300

    Configurando o cors

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const habitRoutes = require('./habitController');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*',            
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'],     //
+}));
+
 app.use(express.json({
     verify: (req, _, buf) => {
         req.rawBody = buf && buf.toString ? buf.toString() : '';
@@ -22,5 +27,5 @@ app.use('/', habitRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando em http://0.0.0.0:${PORT}`);
 });
